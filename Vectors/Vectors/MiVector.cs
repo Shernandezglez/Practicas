dca2976c288f2a4da35aa52af8b1c668fe730266
Shernandezglez <32920669+Shernandezglez@users.noreply.github.com>
2018-03-06 10:12:49 -0600

    Add files via upload

---
+++ Vectors/Vectors/MiVector.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vectors
+{
+    class MiVector
+    {
+        private int[] _v;
+        Random rnd = new Random();
+        
+
+        public MiVector(int tamanio)
+        {
+            _v = new int[tamanio];
+
+        }
+
+        public int llenar()
+        {
+            for(int i = 0; i < _v.Length  ; i++)
+            {
+                _v[i] += i;
+            }
+
+            return _v.Length;
+        }
+
+        public int llenarAlwatorio(int limite)
+        {
+            limite += _v.Length;
+            
+
+            for( int i = 0; i < limite; i++)
+            {
+                _v[i] += rnd.Next(_v.Length);
+            }
+
+            return _v.Length;
+        }
+
+        public string mostrar()
+        {
+            string cosa = "";
+            for (int i = 0; i < _v.Length; i++)
+            {
+                 cosa += "[" + i + "]=" + _v[i].ToString() + Environment.NewLine;
+            }
+
+            return cosa;
+        }
+
+        public string mostrarInverso()
+        {
+            string cosa = "";
+            for (int i = _v.Length-1; i >= 0; i--)
+            {
+               cosa += "[" + i + "]=" + _v[i].ToString() + Environment.NewLine;
+            }
+
+            return cosa;
+        }
+
+        public void sumar()
+        {
+            for (int i = 0; i < _v.Length; i++)
+            {
+                _v[i] = _v.Sum();
+            }
+        }
+
+        public void promediar()
+        {
+            for(int i = 0; i < _v.Length; i++)
+            {
+                
+            }
+        }
+
+
+     
+    }
+}
